f8b8bbfc325c275d1d651ed4e003b74cce294770
Abdelaziz <it@rae.com.sa>
2016-12-26 09:28:22 +0300

    inital

---
+++ bin/fix_file_permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo chmod 777 storage -R
+sudo chmod 600 .env
+sudo chmod 777 bootstrap/cache -R
+#sudo chmod 777 vendor/niklasravnsborg/mpdf/ttfontdata
+#sudo chmod 777 vendor/niklasravnsborg/mpdf/tmp
