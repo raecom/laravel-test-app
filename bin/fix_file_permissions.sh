cce61182e8525e36637498fb800ddf46b3014286
Abdelaziz <it@rae.com.sa>
2016-12-26 10:19:24 +0300

    Make commands compatiable with Shared Hosting

--- bin/fix_file_permissions.sh
+++ bin/fix_file_permissions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo chmod 777 storage -R
-sudo chmod 600 .env
-sudo chmod 777 bootstrap/cache -R
+chmod 777 storage -R
+chmod 600 .env
+chmod 777 bootstrap/cache -R
+
 #sudo chmod 777 vendor/niklasravnsborg/mpdf/ttfontdata
 #sudo chmod 777 vendor/niklasravnsborg/mpdf/tmp
